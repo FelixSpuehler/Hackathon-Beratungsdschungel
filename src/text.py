a59ff8090abe7f494b693154d15f77778a532e15
Florentin Hildebrandt <f.hildebrandt@tu-braunschweig.de>
2020-05-07 15:27:11 +0200

    Added "send form after text page"

--- src/text.py
+++ src/text.py
@@ -12,7 +12,7 @@ class Text(Page):
         # start page block
         script_block = """
         // Page {p_id}
-        var Page{p_id} = form.addPageBreakItem().setTitle("{title}").setHelpText("{formulation}");"""\
+        var Page{p_id} = form.addPageBreakItem().setTitle("{title}").setHelpText("{formulation}").setGoToPage(FormApp.PageNavigationType.SUBMIT);"""\
             .format(p_id=self.p_id, title=self.title, formulation=self.formulation)
         return script_block
 
