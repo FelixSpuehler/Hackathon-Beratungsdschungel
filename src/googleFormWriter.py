8791968320796bc0033fa896ebb398cd6a35590b
Felix Spuehler <felix.spuehler@stud.uni-goettingen.de>
2020-05-07 15:20:35 +0200

    erste googleForm Version

--- src/googleFormWriter.py
+++ src/googleFormWriter.py
@@ -1,16 +1,32 @@
+from datetime import datetime
+
 from page import Page
+
 
 class GoogleFormWriter:
     @staticmethod
-    def write(pages_dict, project_title):
-        start_text = """function createForm() {
+    def write(pages_dict, project_title, file_name=""):
+        print("Writing: Start")
+
+        text_start = """function createForm() {{
         
         // create & name Form
         var item = "{project_title}";
         var form = FormApp.create(item).setTitle(item);""".format(project_title=project_title)
 
-        text_end = "}"
+        text_end = """
+        
+}"""
 
-        print(start_text)
-        print(text_end)
+        if file_name == "":
+            dt_string = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+            file_name = "../data/{}_{}.gs".format(dt_string, project_title)
 
+        with open(file_name, "w") as file:
+            file.write(text_start)
+            for p_id, page in pages_dict.items():
+                file.write(page.create_script_block())
+
+            file.write(text_end)
+
+        print("Writing: End")
--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 from csvReader import CsvReader
+from googleFormWriter import GoogleFormWriter
 
 from question import Question
 from text import Text
 
 if __name__ == '__main__':
     print("Los!")
-    pages = CsvReader.read('../data/Baum_Beispiel.csv')
-    print(len(pages))
-    for key, value in pages.items():
-        print(value)
-        print(value.create_script_block())
+    pages_dict = CsvReader.read('../data/Baum_Beispiel.csv')
+    # print(len(pages))
+    # for key, value in pages.items():
+    #     print(value)
+    #     print(value.create_script_block())
+
+    GoogleFormWriter.write(pages_dict=pages_dict,
+                           project_title="Digitale Studienberatung")
