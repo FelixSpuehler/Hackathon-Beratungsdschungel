667d78c8ba540fd9c4be089a683e0a9f3ffb4e18
charlotte.ackva <charlotte.ackva@stud.uni-goettingen.de>
2020-05-07 14:25:41 +0200

    csv-reader

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 from csvReader import CsvReader
 
-from question import Frage
+from question import Question
 from text import Text
 
 if __name__ == '__main__':
     print("Los!")
+    pages  = CsvReader.read('../data/Baum_Beispiel.csv')
+    print(len(pages))
+    for key, value in pages.items():
+        print(value)
